975346cf4e4e6e1e02d9e8b2a7bbe09e937f41ae
Chris Toma <ctoma@teramind.co>
2026-03-05 14:57:56 +0200

    add tmux-navigator, snacks picker config and showtabline option to nvim

--- omarchy/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua
+++ omarchy/.config/nvim/lua/plugins/snacks-animated-scrolling-off.lua
@@ -4,5 +4,24 @@ return {
 		scroll = {
 			enabled = false, -- Disable scrolling animations
 		},
+		picker = {
+			sources = {
+				explorer = {
+					hidden = true,
+					ignored = true,
+				},
+				grep = {
+					hidden = true,
+					ignored = true,
+				},
+				files = {
+					hidden = true,
+					ignored = true,
+				},
+				git_files = {
+					untracked = true,
+				},
+			},
+		},
 	},
 }
